1ca35e6ed189043c973d8f5d98366c2c15aff92b
Washington Antunes <washington.cead@gmail.com>
2023-03-28 20:35:35 -0300

    paciente controller

--- src/main/java/voll/med/api/controller/PacienteController.java
+++ src/main/java/voll/med/api/controller/PacienteController.java
@@ -1,18 +1,27 @@
 package voll.med.api.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import voll.med.api.paciente.DadosCadastroPaciente;
+import voll.med.api.paciente.Paciente;
+import voll.med.api.paciente.PacienteRepository;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 
 }
--- src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
@@ -1,12 +1,25 @@
 package voll.med.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import voll.med.api.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
         String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
         String cpf,
+        @NotNull
+        @Valid
         DadosEndereco endereco
 ) {
 }
