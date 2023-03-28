496fefb156804ce4d92c3270faaecd17795f4815
Washington Antunes <washington.cead@gmail.com>
2023-03-27 21:03:24 -0300

    criando controller

---
+++ src/main/java/voll/med/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,6 @@
+package voll.med.api.medico;
+
+import voll.med.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
---
+++ src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,12 @@
+package voll.med.api.paciente;
+
+import voll.med.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
+}
