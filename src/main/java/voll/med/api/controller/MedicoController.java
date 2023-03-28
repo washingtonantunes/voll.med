61cf7115f35064f23135fb5b25717a481f774309
Washington Antunes <washington.cead@gmail.com>
2023-03-28 20:08:45 -0300

    atualizando controller

--- src/main/java/voll/med/api/controller/MedicoController.java
+++ src/main/java/voll/med/api/controller/MedicoController.java
@@ -1,17 +1,25 @@
 package voll.med.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import voll.med.api.medico.DadosCadastroMedico;
+import voll.med.api.medico.Medico;
+import voll.med.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
