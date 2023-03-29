b264ccc60ca0c34f39e33098d89442012bbc3216
Washington Antunes <washington.cead@gmail.com>
2023-03-28 21:07:01 -0300

    listagem de medicos

--- src/main/java/voll/med/api/controller/MedicoController.java
+++ src/main/java/voll/med/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package voll.med.api.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import voll.med.api.medico.DadosCadastroMedico;
+import voll.med.api.medico.DadosListagemMedico;
 import voll.med.api.medico.Medico;
 import voll.med.api.medico.MedicoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
