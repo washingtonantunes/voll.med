4bc2e49d5c8c7b95be7980dcc0c34d647ba21303
Washington Antunes <washington.cead@gmail.com>
2023-04-02 17:54:36 -0300

    adicionando filtro

--- src/main/java/voll/med/api/infra/security/TokenService.java
+++ src/main/java/voll/med/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package voll.med.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import voll.med.api.domain.usuario.Usuario;
@@ -19,7 +20,7 @@ public class TokenService {
 
     public String gerarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.HMAC256("secret");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
@@ -27,6 +28,19 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado");
         }
     }
 
